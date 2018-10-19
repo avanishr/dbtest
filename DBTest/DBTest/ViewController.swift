c405122d888f0ea253ae6c4dc20421273d9d51d1
Avanish Rayankula <avanish@epic.com>
2018-10-19 14:27:09 -0500

    make the small view clickable

--- DBTest/DBTest/ViewController.swift
+++ DBTest/DBTest/ViewController.swift
@@ -9,25 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 
     private func setupView() {
-        let newView = UIView()
-        newView.backgroundColor = UIColor.red
-        view.addSubview(newView)
+        addInsertView()
+        //addUpdateView()
+        //addShowContentsView()
+    }
         
-        newView.translatesAutoresizingMaskIntoConstraints = false
-        let horizontalConstraint = newView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        let verticalConstraint = newView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        let widthConstraint = newView.widthAnchor.constraint(equalToConstant: 100)
-        let heightConstraint = newView.heightAnchor.constraint(equalToConstant: 100)
+        
+    private func addInsertView() {
+        let insertView = generateLabel(label: "Insert", textColor: UIColor.white, backgroundColor: UIColor.blue)
+        view.addSubview(insertView)
+        insertView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(insertRecord))
+        insertView.addGestureRecognizer(tap)
+        
+        
+        let horizontalConstraint = insertView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let verticalConstraint = insertView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        let widthConstraint = insertView.widthAnchor.constraint(equalToConstant: 100)
+        let heightConstraint = insertView.heightAnchor.constraint(equalToConstant: 100)
         view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
     }
     
+    
+    private func generateLabel(label: String, textColor: UIColor, backgroundColor: UIColor) -> UILabel {
+        let newLabel = UILabel()
+        newLabel.backgroundColor = backgroundColor
+        newLabel.textColor = textColor
+        newLabel.text = label
+        newLabel.textAlignment = NSTextAlignment.center
+        newLabel.isUserInteractionEnabled = true
+        return newLabel
+    }
+    
+    @objc func insertRecord() {
+        print("record inserted")
+    }
 
 }
 
