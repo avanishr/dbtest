1305057c4bf2e374f25a87e196f6cfc7c7fba4b3
Avanish Rayankula <avanish@epic.com>
2018-10-19 15:52:27 -0500

    add interactions

--- DBTest/DBTest/ViewController.swift
+++ DBTest/DBTest/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var insertView: UILabel?
+    var updateView: UILabel?
+    var removeView: UILabel?
+    var readView: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,27 +21,68 @@ class ViewController: UIViewController {
 
     private func setupView() {
         addInsertView()
-        //addUpdateView()
-        //addShowContentsView()
+        addUpdateView()
+        addRemoveView()
+        addShowContentsView()
     }
         
         
     private func addInsertView() {
-        let insertView = generateLabel(label: "Insert", textColor: UIColor.white, backgroundColor: UIColor.blue)
-        view.addSubview(insertView)
-        insertView.translatesAutoresizingMaskIntoConstraints = false
-        
+        insertView = generateLabel(label: "Insert", textColor: UIColor.white, backgroundColor: UIColor.blue)
         let tap = UITapGestureRecognizer(target: self, action: #selector(insertRecord))
-        insertView.addGestureRecognizer(tap)
+        insertView!.addGestureRecognizer(tap)
         
-        
-        let horizontalConstraint = insertView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        let verticalConstraint = insertView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        let widthConstraint = insertView.widthAnchor.constraint(equalToConstant: 100)
-        let heightConstraint = insertView.heightAnchor.constraint(equalToConstant: 100)
-        view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        view.addSubview(insertView!)
+        insertView!.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraint = insertView!.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+        let middleConstraint = insertView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let widthConstraint = insertView!.widthAnchor.constraint(equalToConstant: 100)
+        let heightConstraint = insertView!.heightAnchor.constraint(equalToConstant: 100)
+        view.addConstraints([topConstraint, middleConstraint, widthConstraint, heightConstraint])
     }
     
+    
+    private func addUpdateView() {
+        updateView = generateLabel(label: "Update", textColor: UIColor.black, backgroundColor: UIColor.green)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(updateRecord))
+        updateView!.addGestureRecognizer(tap)
+        
+        view.addSubview(updateView!)
+        updateView!.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraint = updateView!.topAnchor.constraint(equalTo: insertView!.bottomAnchor, constant: 20)
+        let middleConstraint = updateView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let widthConstraint = updateView!.widthAnchor.constraint(equalToConstant: 100)
+        let heightConstraint = updateView!.heightAnchor.constraint(equalToConstant: 100)
+        view.addConstraints([topConstraint, middleConstraint, widthConstraint, heightConstraint])
+    }
+    
+    private func addRemoveView() {
+        removeView = generateLabel(label: "Remove", textColor: UIColor.white, backgroundColor: UIColor.red)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(removeRecord))
+        removeView!.addGestureRecognizer(tap)
+        
+        view.addSubview(removeView!)
+        removeView!.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraint = removeView!.topAnchor.constraint(equalTo: updateView!.bottomAnchor, constant: 20)
+        let middleConstraint = removeView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let widthConstraint = removeView!.widthAnchor.constraint(equalToConstant: 100)
+        let heightConstraint = removeView!.heightAnchor.constraint(equalToConstant: 100)
+        view.addConstraints([topConstraint, middleConstraint, widthConstraint, heightConstraint])
+    }
+    
+    private func addShowContentsView() {
+        readView = generateLabel(label: "Display", textColor: UIColor.white, backgroundColor: UIColor.brown)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(displayRecords))
+        readView!.addGestureRecognizer(tap)
+        
+        view.addSubview(readView!)
+        readView!.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraint = readView!.topAnchor.constraint(equalTo: removeView!.bottomAnchor, constant: 20)
+        let middleConstraint = readView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let widthConstraint = readView!.widthAnchor.constraint(equalToConstant: 100)
+        let heightConstraint = readView!.heightAnchor.constraint(equalToConstant: 100)
+        view.addConstraints([topConstraint, middleConstraint, widthConstraint, heightConstraint])
+    }
     
     private func generateLabel(label: String, textColor: UIColor, backgroundColor: UIColor) -> UILabel {
         let newLabel = UILabel()
@@ -51,6 +96,18 @@ class ViewController: UIViewController {
     
     @objc func insertRecord() {
         print("record inserted")
+    }
+    
+    @objc func updateRecord() {
+        print("record updated")
+    }
+    
+    @objc func removeRecord() {
+        print("record deleted")
+    }
+    
+    @objc func displayRecords() {
+        print("records displayed")
     }
 
 }
